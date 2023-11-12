f94f20d53a4fc833a5ce5e0d2bd8f91ddb682385
Dag Reuterskiöld <dsterna@kth.se>
2023-11-12 22:06:11 +0100

    sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,67 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
+import gz from "./assets/gz.mp3";
 import "./App.css";
+import Fireworks from "@fireworks-js/react";
+import useSound from "use-sound";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isRunning, setisRunning] = useState(false);
+  const [play, { stop }] = useSound(gz, { volume: 0.5 });
+  useEffect(() => {
+    if (!ref.current) return;
+    if (!isRunning) {
+      ref.current.stop();
+    } else {
+      play();
+      setTimeout(() => {
+        ref.current.start();
+      }, 1000);
+    }
+  }, [isRunning]);
 
+  const toggle = () => {
+    if (!ref.current) return;
+    if (ref.current.isRunning) {
+      ref.current.stop();
+    } else {
+      ref.current.start();
+    }
+  };
+
+  const ref = useRef(null);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="container">
+        <div
+          style={{
+            display: "flex",
+            gap: "4px",
+            position: "absolute",
+            zIndex: 1,
+          }}
+        >
+          {!isRunning && (
+            <button onClick={() => setisRunning(true)}>
+              Är det din födelsedag?
+            </button>
+          )}
+        </div>
+        <Fireworks
+          ref={ref}
+          options={{ opacity: 0.5 }}
+          style={{
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            background: "#000",
+          }}
+        />
+        <div id="overlay">GJATTIS /A</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>Hej</div>
     </>
   );
 }
